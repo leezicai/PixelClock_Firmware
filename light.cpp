8a672a5d0566e40cd66222f1adcf073ec7d4cdd4
Lizicai <shaoxin5345@gmail.com>
2024-09-18 21:50:23 +0800

    以注释方式增加2种屏幕排列

--- light.cpp
+++ light.cpp
@@ -69,7 +69,8 @@ arduinoFFT FFT = arduinoFFT(vReal, vImag, SAMPLES, SAMPLING_FREQ);
 int model2ColorArray[2][3] = {{0, 220, 255}, {240, 45, 255}};
 int model4ColorArrar[8][3] = {{240, 45, 255},{253, 98, 248},{253, 169, 205},{255, 196, 123},{253, 214, 200},{253, 192, 255},{249, 175, 255},{0, 220, 255}};
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, MATRIX_SIDE, 1, 1, DATAPIN,
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, 4, 1, DATAPIN,
+// Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, 1, 1, DATAPIN,
 NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE + 
 NEO_TILE_TOP + NEO_TILE_LEFT + NEO_TILE_ROWS + NEO_TILE_PROGRESSIVE, NEO_GRB + NEO_KHZ800);
 
